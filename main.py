d476a575aab3df8d5638cccc7a03c4988058833b
krasilnikov-dimchik <krasilnikov.dimchik@gmail.com>
2021-11-28 10:55:34 +0300

    Create a python environment

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 # Reference to module documentation https://dev.mysql.com/doc/connector-python/en/connector-python-reference.html
 import mysql.connector
 from getpass import getpass
-
 from mysql.connector.errors import Error
-auth_plugin = "plugin_name"
+
+
 def connection():
+    # Create a new connection to MySQL server
     print("Connect to a mysql server ...")
     try:
-        # getpass.unix_getpass()
-        conn = mysql.connector.connect(user= "dimchik", host="localhost", password= getpass(prompt="Enter a password:\n",))
-        # Reports whether the connection to MySQL Server is available
-        if conn.is_connected():
+        user = input("Enter a username:\n")
+        connect = mysql.connector.connect(user=user, host="localhost", password=getpass(prompt="Enter a password:\n",))
+        if connect.is_connected():
             print("Connect to a mysql server: OK")
     except Error as err:
         raise err
-    return conn
+    return connect
+
 
 if __name__ == "__main__":
     conn = connection()
